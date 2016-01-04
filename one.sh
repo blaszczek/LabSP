5d6c48414496c5a56ee672a1b243f59cc1d386ac
Rafał Błaszkiewicz <blaszczek@outlook.com>
2016-01-04 21:45:04 +0100

    wykonano zmiany

---
+++ one.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo $LOGNAME
+<<<<<<< HEAD
+date
+=======
+>>>>>>> master
+echo "dowolny komunikat wypisany na ekran"
+echo "dowolny komunikat wypisany na ekran"
+echo "dowolny komunikat wypisany na ekran"
+
